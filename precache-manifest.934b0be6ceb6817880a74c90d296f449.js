093068804a44c9f7c7a662d4d660016b798ac704
Gabriele Gonçalves Vieira <gabriele.vieira@shopee.com>
2023-01-30 16:31:48 -0300

    Updates

---
+++ precache-manifest.934b0be6ceb6817880a74c90d296f449.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bbb1c64e0c9f876a96d3",
+    "url": "/digital-resume/static/css/main.5246b804.chunk.css"
+  },
+  {
+    "revision": "bbb1c64e0c9f876a96d3",
+    "url": "/digital-resume/static/js/main.bbb1c64e.chunk.js"
+  },
+  {
+    "revision": "ce0a7660c2f31c52c532",
+    "url": "/digital-resume/static/js/runtime~main.ce0a7660.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/digital-resume/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "519511e784fa7e11efa03daf96c10481",
+    "url": "/digital-resume/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/digital-resume//precache-manifest.0a826609ebaccb12ae70f9499ef5d1fc.js"
+  "/digital-resume/precache-manifest.934b0be6ceb6817880a74c90d296f449.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/digital-resume//index.html", {
+workbox.routing.registerNavigationRoute("/digital-resume/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
